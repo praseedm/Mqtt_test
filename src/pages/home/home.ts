c6d08f651d03a7a172cce2022393485ab1ee3c51
agin <agin@softwareassociates.in>
2018-06-29 16:20:12 +0530

    demo message removed

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
 
 import {MqttService, IMqttMessage} from 'ngx-mqtt';
 import { Subscription } from 'rxjs'
@@ -14,37 +15,58 @@ export class HomePage implements OnDestroy {
   author : string = "app : ";
   publishM : string = "";
   messageArray = [];
-  temp: string="hai";
+  //temp: string="hai";
 
   private subs : Subscription;
 
   constructor(public navCtrl: NavController,
-    private mqttService: MqttService) {
-
-    console.log(this.mqttService);
-    
-    this.messageArray.push(this.temp);
-
+    private mqttService: MqttService, private _toastC : ToastController) {
+    console.log(this.mqttService);     
   }
 
   public subscribe( topic : string){
 
+    if(this.subs){
+      this.unsubscribe();
+    }
     this.messageArray = [];
+
     this.subs = this.mqttService.observe(topic).subscribe(
       (message : IMqttMessage) => {
         console.log(message);
+        this.showToast();
         this.message = message.payload.toString();
         this.messageArray.push(this.message);
-      }
-    );
+      });
+
+  }
+
+  public showToast(){
+    let toast = this._toastC.create({
+      message: 'new message',
+      duration: 2000,
+      position: 'bottom'
+    });
+  
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+  
+    toast.present();
+
   }
 
   public publish(topic:string, publishM:String) {
     this.mqttService.unsafePublish(topic, this.author + publishM);
   }
 
-  public ngOnDestroy() {
+  public unsubscribe(){
+    console.log("unsubscribe");
     this.subs.unsubscribe();
+  }
+
+  public ngOnDestroy() {
+    this.unsubscribe();
   }
 
 }
